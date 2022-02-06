53cc27caaa035e77ccdb714fea6e3316484333ef
simealdana <simeon@wearecapicua.com>
2022-02-06 19:06:51 -0300

    fix table view

--- components/heroAlternate/index.tsx
+++ components/heroAlternate/index.tsx
@@ -3,11 +3,26 @@ import { styles } from "./style";
 import { Button } from "../shared/button";
 import { ImageWithSketelon } from "../shared/Image";
 import { messages } from "./messages";
+import { useEffect, useState } from "react";
 
 interface HeroInterfaces {
   breackPoint: BreakPoints;
 }
+const INITAL_SIZE:number = 500;
+
 export const HeroAlternate = ({ breackPoint }: HeroInterfaces) => {
+  const [imgSize,setImgSize]=useState<number>(INITAL_SIZE);
+
+  const handleSettingImgSize = ()=>{
+    console.log(breackPoint === BreakPoints.MD);
+    if( breackPoint === BreakPoints.MD){
+      setImgSize(300)
+    }
+  }
+
+  useEffect(()=>{
+    handleSettingImgSize();
+  },[breackPoint])
   const handleClick = () => {
     window.open("https://discord.com/", "_blank");
   };
@@ -16,8 +31,8 @@ export const HeroAlternate = ({ breackPoint }: HeroInterfaces) => {
         <div className="alternate_hero__content">
             <ImageWithSketelon
                 src="https://res.cloudinary.com/doa8qp6ca/image/upload/v1644170516/capybaras/CAPYillustration_3_2_vwxrl5.png"
-                width={500}
-                height={500}
+                width={imgSize}
+                height={imgSize}
                 alt="test"
             />
             <Button
--- components/navBarAlternate/style.ts
+++ components/navBarAlternate/style.ts
@@ -3,8 +3,8 @@ export const style ={
     listMobileOpen:'navBarList__mobile text-secondary cursor-pointer bg-menu-background', 
     listMobileClose:'navBarList__mobile__close text-secondary cursor-pointer bg-menu-background', 
     navBar:'sticky bg-primary-background',
-    navBarContainer:'flex justify-between  p-5 items-center container',
-    navBarMobileOpen:'flex justify-between bg-menu-background p-5 items-center',
+    navBarContainer:'flex justify-between  p-5 md:p-1 items-center container',
+    navBarMobileOpen:'flex justify-between bg-menu-background items-center',
     button:'border-solid text-primary p-2 rounded-2xl border-button'
 }
 
