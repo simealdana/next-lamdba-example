3e89fef9d4a3a202e48e86203862e6d26fe4031a
simealdana <simeon@wearecapicua.com>
2022-02-06 17:19:31 -0300

    added new text bottom of the caruosel

--- components/GridAlternate/index.tsx
+++ components/GridAlternate/index.tsx
@@ -1,3 +1,4 @@
+import { messages } from "./messages";
 import { GridList } from "./components/gridList";
 import { ITEMS } from "./constans";
 import { styles } from "./styles";
@@ -5,5 +6,6 @@ import { styles } from "./styles";
 export const GridAlternate = () => (
   <div className={`${styles.alternateList}`}>
     <GridList items={ITEMS} classes="flex" />
+    <p className="text-center text-primary">{messages.text}</p>
   </div>
 );
---
+++ components/GridAlternate/messages.ts
@@ -0,0 +1,3 @@
+export const messages ={
+    text:'THESE ARE PREVIEWS, NOT ACTUAL TOKENS.'
+}
